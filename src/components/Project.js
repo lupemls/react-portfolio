dd58c3d5e1b1bc1062f3e2aab10678c5498b4d20
Matt Shepardson <lupemls@yahoo.com>
2020-06-03 01:37:49 -0700

    Added content for Contact Page

---
+++ src/components/Project.js
@@ -0,0 +1,46 @@
+import React from "react";
+import images from "../../public/assets/images";
+
+function Project () {
+    const projects = [
+        {
+            name: "Atari Portal",
+            image: "images/atari-portal",
+            repo: "https://github.com/lupemls/first-project",
+            deployed: "https://lupemls.github.io/first-project"
+        },
+        {
+            name: "RPS Kombat",
+            image: "images/rps-kombat.jpg",
+            repo: "https://github.com/lupemls/project-2",
+            deployed: "https://still-peak-87577.herokuapp.com/"
+        },
+        {
+            name: "Day Planner",
+            image: "images/day-planner.png",
+            repo: "https://github.com/lupemls/day-planner",
+            deployed: "https://lupemls.github.io/day-planner"
+        },
+        {
+            name: "Weather Dashboard",
+            image: "images/weather-dashboard.png",
+            repo: "https://github.com/lupemls/weather-dashboard",
+            deployed: "https://lupemls.github.io/weather-dashboard"
+        },
+        {
+            name: "Note Taker",
+            image: "images/note-taker.jpg",
+            repo: "https://github.com/lupemls/note-taker",
+            deployed: ""
+        },
+        {
+            name: "Arcade Quest",
+            image: "images/arcade-quest.png",
+            repo: "https://github.com/lupemls/arcade-quest",
+            deployed: "https://nameless-plains-98133.herokuapp.com/"
+        },
+
+    ]
+}
+
+export default Project;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,41 @@
 import React from "react";
 
-function Contact () {
-    return (
-        <div>
-            
+function Contact() {
+  return (
+    <div>
+      <section class="p-3 border">
+        <h2 class="mb-3 pb-3">Contact</h2>
+        <div class="form-group">
+          <label for="exampleInputName">Name</label>
+          <input
+            type="name"
+            class="form-control"
+            id="inputName"
+            aria-describedby="nameHelp"
+          />
         </div>
-    )
+        <div class="form-group">
+          <label for="exampleInputEmail">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="inputEmail"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputName">Message</label>
+          <input
+            type="message"
+            class="form-control"
+            id="inputMessage"
+            aria-describedby="messageHelp"
+          />
+        </div>
+        <input class="btn btn-primary" type="submit" value="Submit" />
+      </section>
+    </div>
+  );
 }
 
 export default Contact;
